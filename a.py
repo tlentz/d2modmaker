89cd4c392125fe726a7e787ddac4f202b2fe12ea
Tyler Lentz <tyler.cg.lentz@gmail.com>
2020-06-07 14:20:23 -0500

    init

---
+++ a.py
@@ -0,0 +1,28 @@
+import csv
+import pandas as pd
+
+src_data = {}
+src_files = [
+    "Gems",
+    "Runes",
+    "SetItems",
+    "Sets",
+    "UniqueItems"
+]
+
+
+def read_src_file(fname):
+    csvfile = f"./source/{fname}.txt"
+    df = pd.read_csv(csvfile, delimiter="\t", engine="python")
+    src_data[fname] = df
+
+
+def main():
+    for f in src_files:
+        read_src_file(f)
+    print(src_data)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
