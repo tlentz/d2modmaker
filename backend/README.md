aca6a75c798ae3673a735b7299ec1af58df316b1
Tyler Lentz <tyler.cg.lentz@gmail.com>
2020-06-09 09:48:58 -0500

    adding options

---
+++ backend/README.md
@@ -0,0 +1,6 @@
+# D2Randomizer
+
+## Options
+* Remove class specific requirements for +skills
+    * change `skill` with `oskill`
+    
---
+++ backend/src/config.ts
@@ -0,0 +1,8 @@
+class Config {
+    removeClassSpecificSkills: boolean;
+    randomizeUniques: boolean;
+    constructor(removeClassSpecificSkills: boolean, randomizeUniques: boolean) {
+        this.removeClassSpecificSkills = removeClassSpecificSkills;
+        this.randomizeUniques = randomizeUniques;
+    }
+}
