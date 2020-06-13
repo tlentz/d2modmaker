0291be7ac8e8335c93f11fb71955b04cc52b8a0c
Tyler Lentz <tyler.cg.lentz@gmail.com>
2020-06-13 11:48:54 -0500

    add flask api

---
+++ api/api.py
@@ -0,0 +1,14 @@
+import flask
+from flask import request, jsonify
+from models.d2data import D2Data
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+data = D2Data()
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify(data.variable)
+
+app.run()
---
+++ api/models/d2data.py
@@ -0,0 +1,5 @@
+class D2Data:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
