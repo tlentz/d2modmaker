69a672e5fb1e2d373536de8258e44dadeac5a605
Tyler Lentz <tyler.cg.lentz@gmail.com>
2020-06-15 20:45:10 -0500

    Update d2data.py

--- api/models/d2data.py
+++ api/models/d2data.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import json
+import pandas
 
 def get_data_dir():
     this_dir = os.path.dirname(os.path.realpath('__file__'))
@@ -29,19 +30,19 @@ class D2File:
         self.data = []
 
         self.read()
-#         self.write()
+        self.write()
 
     def read(self):
         self.data = []
         print (self.name)
-        with open(self.file_name, newline='\r\n', encoding="cp1252") as csvfile:
+        with open(self.file_name, newline='\r\n', encoding="Windows-1252") as csvfile:
             reader = csv.DictReader(f=csvfile, delimiter='\t')
             self.fieldnames = reader.fieldnames
             for row in reader:
                 self.data.append(row)
 
     def write(self):
-        with open(self.file_name, 'w', newline='\r\n', encoding="cp1252") as csvfile:
+        with open(self.file_name, 'w', newline='\r\n', encoding="Windows-1252") as csvfile:
            writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames, delimiter='\t')
            writer.writeheader()
            writer.writerows(self.data)
