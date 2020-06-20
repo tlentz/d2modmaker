2b2d104b3e8191cf3e134e4ae83dff330c03042e
Tyler Lentz <tyler.cg.lentz@gmail.com>
2020-06-20 12:29:01 -0500

    test uniqueitemx.txt

--- go-api/main.go
+++ go-api/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	//"bufio"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("hi")
+	// Open the file
+	csvfile, err := os.Open("../assets/113c-data/UniqueItems.txt")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(csvfile)
+	r.Comma = '\t'
+	//r := csv.NewReader(bufio.NewReader(csvfile))
+
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(record)
+	}
 }
