187e18238a24c2a3ee02eaf12a8b6ba34d27530b
Tyler Lentz <tyler.cg.lentz@gmail.com>
2020-06-23 10:17:41 -0500

    Update main.go

--- cmd/d2modmaker/main.go
+++ cmd/d2modmaker/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"strconv"
-
 	"github.com/tlentz/d2modmaker/internal/d2file"
+	levels "github.com/tlentz/d2modmaker/internal/levelsTxt"
 	skills "github.com/tlentz/d2modmaker/internal/skillsTxt"
 	"github.com/tlentz/d2modmaker/internal/util"
 )
@@ -37,16 +36,23 @@ func enableTownTeleport(d2files *d2file.D2Files) {
 	}
 }
 
-func increaseMonsterDensity(d2files *d2file.D2Files, int m) {
+func increaseMonsterDensity(d2files *d2file.D2Files, m int) {
 	f := d2file.GetOrCreateFile(dataDir, d2files, levels.FileName)
-	var monDen = "MonDen"
-	var monUMin = "MonUMin"
-	var monUMax = "MonUMax"
-	var maxDensity = 10000
-	var maxM = 30
-	multiplier := Min(maxM, m)
-	var nmOffset = 1
-	var hellOffset = 2
+	maxM := 30
+	multiplier := util.Min(maxM, m)
+	maxDensity := 10000
+
+	for idx, row := range f.Rows {
+
+	}
+	// var monDen = "MonDen"
+	// var monUMin = "MonUMin"
+	// var monUMax = "MonUMax"
+	// var maxDensity = 10000
+	// var maxM = 30
+	// multiplier := Min(maxM, m)
+	// var nmOffset = 1
+	// var hellOffset = 2
 
 	// difficulty offsets norm, nm, hell
 	// var difficultyOffsets = []int{0, 1, 2}}
@@ -68,30 +74,30 @@ func increaseMonsterDensity(d2files *d2file.D2Files, int m) {
 	// 	}
 	// }
 
-	var difficulties = []string{"", "(N)", "(H)"}
-	for idx, _ := range d2file.Records {
-		for _, diff := range difficulties {
+	// var difficulties = []string{"", "(N)", "(H)"}
+	// for idx, _ := range d2file.Records {
+	// 	for _, diff := range difficulties {
 
-			// MonDen
-			oldMonDen, err := strconv.Atoi(d2file.Records[idx][monDen+diff])
-			if err == nil {
-				newMonDen := Min(maxDensity, oldMonDen*multiplier)
-				d2file.Records[idx][monDen+diff] = strconv.Itoa(newMonDen)
-			}
+	// 		// MonDen
+	// 		oldMonDen, err := strconv.Atoi(d2file.Records[idx][monDen+diff])
+	// 		if err == nil {
+	// 			newMonDen := Min(maxDensity, oldMonDen*multiplier)
+	// 			d2file.Records[idx][monDen+diff] = strconv.Itoa(newMonDen)
+	// 		}
 
-			// MonUMin
-			oldMonUMin, err := strconv.Atoi(d2file.Records[idx][monUMin+diff])
-			if err == nil {
-				newMonUMin := oldMonUMin * multiplier
-				d2file.Records[idx][monUMin+diff] = strconv.Itoa(newMonUMin)
-			}
+	// 		// MonUMin
+	// 		oldMonUMin, err := strconv.Atoi(d2file.Records[idx][monUMin+diff])
+	// 		if err == nil {
+	// 			newMonUMin := oldMonUMin * multiplier
+	// 			d2file.Records[idx][monUMin+diff] = strconv.Itoa(newMonUMin)
+	// 		}
 
-			// MonUMax
-			oldMonUMax, err := strconv.Atoi(d2file.Records[idx][monUMax+diff])
-			if err == nil {
-				newMonUMax := oldMonUMax * multiplier
-				d2file.Records[idx][monUMax+diff] = strconv.Itoa(newMonUMax)
-			}
-		}
-	}
+	// 		// MonUMax
+	// 		oldMonUMax, err := strconv.Atoi(d2file.Records[idx][monUMax+diff])
+	// 		if err == nil {
+	// 			newMonUMax := oldMonUMax * multiplier
+	// 			d2file.Records[idx][monUMax+diff] = strconv.Itoa(newMonUMax)
+	// 		}
+	// 	}
+	// }
 }
