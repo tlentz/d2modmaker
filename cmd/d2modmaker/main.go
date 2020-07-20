876600c2fd31478b149b48b153cfb418eafda5c2
Tyler Lentz <tyler.cg.lentz@gmail.com>
2020-07-19 19:57:38 -0500

    printFile helper

--- cmd/d2modmaker/main.go
+++ cmd/d2modmaker/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/tlentz/d2modmaker/internal/d2file"
+	"github.com/tlentz/d2modmaker/internal/superUniquesTxt"
 )
 
 var (
@@ -33,4 +36,12 @@ func main() {
 	fmt.Println(line)
 
 	makeMod()
+}
+
+func printFile() {
+	d2files := d2file.D2Files{}
+	f := d2file.GetOrCreateFile(dataDir, d2files, superUniquesTxt.FileName)
+	for i := range f.Headers {
+		fmt.Println(f.Headers[i], " = ", i)
+	}
 }
