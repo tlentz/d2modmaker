6001775c2d8dbd80b24dd6b0354c2f9e8d962fda
Tyler Lentz <tyler.cg.lentz@gmail.com>
2020-07-23 02:10:51 -0500

    WIP: form

--- gui/react-ui/src/app/store.ts
+++ gui/react-ui/src/app/store.ts
@@ -1,9 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import { reducer as reduxFormReducer } from "redux-form";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    form: reduxFormReducer,
   },
 });
 
