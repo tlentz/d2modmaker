27ef2ee111c140d4d6e7dbd522439929a94f148b
Tyler Lentz <tyler.cg.lentz@gmail.com>
2020-07-23 02:14:58 -0500

    Revert "WIP: form"
    This reverts commit 6001775c2d8dbd80b24dd6b0354c2f9e8d962fda.

--- gui/react-ui/src/app/store.ts
+++ gui/react-ui/src/app/store.ts
@@ -1,11 +1,9 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
-import { reducer as reduxFormReducer } from "redux-form";
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    form: reduxFormReducer,
   },
 });
 
