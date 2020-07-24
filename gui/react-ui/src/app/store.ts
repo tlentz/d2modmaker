f40ce6c1276c14a263cd0dbf3e4e3c82daa81226
Tyler Lentz <tyler.cg.lentz@gmail.com>
2020-07-23 22:21:46 -0500

    WIP setting up react

--- gui/react-ui/src/app/store.ts
+++ gui/react-ui/src/app/store.ts
@@ -1,9 +1,7 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
   },
 });
 
--- gui/react-ui/src/index.tsx
+++ gui/react-ui/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './app/App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
