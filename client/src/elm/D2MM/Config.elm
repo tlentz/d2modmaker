e429bb51d09ce15f771d2c9575cf4098cfcbad0b
tlentz <tyler.cg.lentz@gmail.com>
2021-01-23 17:28:40 -0600

    Update Config.elm

--- client/src/elm/D2MM/Config.elm
+++ client/src/elm/D2MM/Config.elm
@@ -2,4 +2,34 @@ module D2MM.Config exposing (..)
 
 
 type alias Config =
-    { placeholder : String }
+    { version : String
+    , sourceDir : String
+    , outputDir : String
+    , meleeSplash : Bool
+    , increasedStackSizes : Bool
+    , increaseMonsterDensity : Int
+    , enableTownSkills : Bool
+    , noDropZero : Bool
+    , questDrops : Bool
+    , uniqueItemDropRate : Float
+    , runeDropRate : Float
+    , startWithCube : Bool
+    , cowzzz : Bool
+    , removeLevelRequirements : Bool
+    , removeAttRequirements : Bool
+    , removeUniqCharmLimit : Bool
+    , randomOptions : RandomOptions
+    }
+
+
+type alias RandomOptions =
+    { randomize : Bool
+    , seed : Int
+    , isBalanced : Bool
+    , balancedPropCount : Bool
+    , allowDuplicateProps : Bool
+    , minProps : Int
+    , maxProps : Int
+    , useOSkills : Bool
+    , perfectProps : Bool
+    }
