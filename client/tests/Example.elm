0aef7d2e512b9a11bb3a5e55213bfb5469f77a67
tlentz <tyler.cg.lentz@gmail.com>
2021-01-23 16:25:51 -0600

    Update Example.elm

--- client/tests/Example.elm
+++ client/tests/Example.elm
@@ -1,11 +1,7 @@
-module Example exposing (fuzzTest, unitTest, viewTest)
+module Example exposing (..)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
-import Main exposing (..)
 import Test exposing (..)
-import Test.Html.Query as Query
-import Test.Html.Selector exposing (tag, text)
 
 
 {-| See <https://github.com/elm-community/elm-test>
@@ -13,40 +9,7 @@ import Test.Html.Selector exposing (tag, text)
 unitTest : Test
 unitTest =
     describe "simple unit test"
-        [ test "Inc adds one" <|
+        [ test "1 + 1 = 2" <|
             \() ->
-                update Inc (Model 0 "")
-                    |> Tuple.first
-                    |> .counter
-                    |> Expect.equal 1
-        ]
-
-
-{-| See <https://github.com/elm-community/elm-test>
--}
-fuzzTest : Test
-fuzzTest =
-    describe "simple fuzz test"
-        [ fuzz int "Inc ALWAYS adds one" <|
-            \ct ->
-                update Inc (Model ct "")
-                    |> Tuple.first
-                    |> .counter
-                    |> Expect.equal (ct + 1)
-        ]
-
-
-{-| see <https://github.com/eeue56/elm-html-test>
--}
-viewTest : Test
-viewTest =
-    describe "Testing view function"
-        [ test "Button has the expected text" <|
-            \() ->
-                Model 0 ""
-                    |> view
-                    |> Query.fromHtml
-                    |> Query.findAll [ tag "button" ]
-                    |> Query.first
-                    |> Query.has [ text "+ 1" ]
+                1 + 1 |> Expect.equal 2
         ]
